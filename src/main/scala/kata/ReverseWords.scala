cc3e84875f90fd672c74eb75aa97b1cae7541af7
Mahesh Chand <mahesh.kndpl@gmail.com>
2019-09-01 11:42:53 +0530

    added problem statement

--- src/main/scala/kata/ReverseWords.scala
+++ src/main/scala/kata/ReverseWords.scala
@@ -1,5 +1,14 @@
 package kata
 
+/* Problem: Reversed Words
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+
+reverseWords("The greatest victory is that which requires no battle")
+// "battle no requires which that is victory greatest The"
+
+ */
 object ReverseWords {
 
   def reverseWords(str: String): String = str.split(' ').reverse.mkString(" ")
--- src/main/scala/kata/SplitStrings.scala
+++ src/main/scala/kata/SplitStrings.scala
@@ -1,5 +1,14 @@
 package kata
 
+/* Problem: Split Strings
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an
+odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+Examples:
+
+solution('abc')    // should return List("ab", "c_")
+solution('abcdef') // should return List("ab", "cd", "ef")
+
+ */
 object SplitStrings {
 
   def solution(s: String): List[String] = {
